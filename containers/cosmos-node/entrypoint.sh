6f0c657baa326f1bc16090d7ecd02feaec592f04
Kien Nguyen <kien.nguyen@ledger.fr>
2024-06-10 20:09:38 +0200

    build: add logging in entrypoint

--- containers/cosmos-node/entrypoint.sh
+++ containers/cosmos-node/entrypoint.sh
@@ -5,17 +5,21 @@ MONIKER=cosmos-4
 
 if [ ! -d "$HOME/data" ] || [ -z "$(ls -A $HOME/data)" ]; then
     # Init
+    echo -e "\e[32m[+]\e[0m Init config"
     gaiad init $MONIKER --home $HOME
 
     # Download genesis file
+    echo -e "\e[32m[+]\e[0m Download genesis file"
     wget -nv https://raw.githubusercontent.com/cosmos/mainnet/master/genesis/genesis.cosmoshub-4.json.gz
     gzip -d genesis.cosmoshub-4.json.gz
     mv genesis.cosmoshub-4.json $HOME/config/genesis.json
 
     # Configure seeds and peers
+    echo -e "\e[32m[+]\e[0m Setup addrbook.json"
     wget -nv -O $HOME/config/addrbook.json https://dl2.quicksync.io/json/addrbook.cosmos.json
 
     # Configure gas price
+    echo -e "\e[32m[+]\e[0m Setup minimum gas price"
     sed -i 's|^minimum-gas-prices =.*|minimum-gas-prices = "0.0025uatom"|g' $HOME/config/app.toml
 
     # Configure pruning of state
@@ -23,16 +27,18 @@ if [ ! -d "$HOME/data" ] || [ -z "$(ls -A $HOME/data)" ]; then
     sed -i 's|^pruning =.*|pruning = "default"|g' $HOME/config/app.toml
 
     # Enable prometheus
+    echo -e "\e[32m[+]\e[0m Enable prometheus monitoring"
     sed -i 's|^prometheus =.*|prometheus = "true"|g' $HOME/config/config.toml
     sed -i 's|^max_open_connections =.*|max_open_connections = 100|g' $HOME/config/config.toml
 
     # Download pruned snapshot
     # Or check in https://polkachu.com/tendermint_snapshots/cosmos for 15Gi snapshot
-    wget -O cosmoshub-4-pruned.tar.lz4 https://dl2.quicksync.io/cosmoshub-4-pruned.$(date +'%Y%m%d').0310.tar.lz4 
-    lz4 -d cosmoshub-4-pruned.tar.lz4 -o cosmoshub-4-pruned
-    tar -xf cosmoshub-4-pruned -C $HOME/config/data/ --strip-components=1
-    rm cosmoshub-4-pruned.lz4 cosmoshub-4-pruned
+    echo -e "\e[32m[+]\e[0m Downloading snapshot"
+    wget -nv -O cosmos.tar.lz4 https://snapshots.polkachu.com/snapshots/cosmos/cosmos_20801917.tar.lz4 
+    lz4 -d cosmos.tar.lz4
+    tar -xf cosmos.tar -C $HOME/config/data --strip-components=1
 fi
 
 # Start
+echo -e "\e[32m[+]\e[0m Start gaia ..."
 gaiad start --home $HOME 
--- containers/polygon-node/entrypoint.sh
+++ containers/polygon-node/entrypoint.sh
@@ -1,21 +1,27 @@
+#!/bin/bash
+
 HOME=$CONFIG_DIR/heimdalld
 
 # if data dir not exist, init config and download snapshot
 if [ ! -d "$HOME/data" ] || [ -z "$(ls -A $HOME/data)" ]; then
     # Init heimdalld
+    echo -e "\e[32m[+]\e[0m Init config"
     heimdalld init --chain mainnet --home $HOME
 
     # Get Genesis.json with sentry
+    echo -e "\e[32m[+]\e[0m Download genesis file"
     wget -O $HOME/config/genesis.json https://raw.githubusercontent.com/maticnetwork/launch/master/mainnet-v1/sentry/sentry/heimdall/config/genesis.json
 
     # Configure seeds and peers
     ## see: https://docs.polygon.technology/pos/how-to/full-node/full-node-binaries/#configure-heimdall-seeds-mainnet
     ## see: https://github.com/maticnetwork/node-ansible/blob/master/roles/heimdall/install-heimdall/tasks/main.yml
+    echo -e "\e[32m[+]\e[0m Setup seeds and peers"
     sed -i 's|^seeds =.*|seeds = "1500161dd491b67fb1ac81868952be49e2509c9f@52.78.36.216:26656,dd4a3f1750af5765266231b9d8ac764599921736@3.36.224.80:26656,8ea4f592ad6cc38d7532aff418d1fb97052463af@34.240.245.39:26656,e772e1fb8c3492a9570a377a5eafdb1dc53cd778@54.194.245.5:26656,6726b826df45ac8e9afb4bdb2469c7771bd797f1@52.209.21.164:26656"|g' $HOME/config/config.toml
     sed -i 's|^persistent_peers =.*|persistent_peers = "1500161dd491b67fb1ac81868952be49e2509c9f@52.78.36.216:26656,dd4a3f1750af5765266231b9d8ac764599921736@3.36.224.80:26656,82f3085a83faa522c3cafa4e4dce1ef3a0c660f3@13.209.168.182:26656,f2b1ba3a684c4705aff7c01ec1c454a39794db5a@43.201.242.62:26656,bb98b9abd23a9d2e196b2c8a03cfb51a4bd49b47@43.202.78.165:26656,5606200cbdf662620625edea63b6a4275128fb34@3.38.254.221:26656,d76001510004c802fd2977488eb753ef261a245b@15.165.197.16:26656,e56dbf76c7b9508ecece447195382301e7c90ec7@52.78.154.236:26656,e772e1fb8c3492a9570a377a5eafdb1dc53cd778@54.194.245.5:26656,8ea4f592ad6cc38d7532aff418d1fb97052463af@34.240.245.39:26656,6726b826df45ac8e9afb4bdb2469c7771bd797f1@52.209.21.164:26656,ec59b724d669b9df205156e8fd457257116b1745@99.81.158.129:26656,08de3da03bd6774e4c5464dd29ddedddefbb1907@34.254.124.45:26656,78610e49cd8efb28c835c8478b30cf94650335b9@34.252.116.193:26656,e6816ab7fc88522be49940287206391bde87eeb9@54.76.109.39:26656,3215b1cf88ea913f477c0db0be00fb873d826d72@34.246.232.184:26656"|g' $HOME/config/config.toml
 
     # Enable prometheus
     ## see: https://docs.polygon.technology/pos/how-to/full-node/full-node-binaries/#configure-heimdall-seeds-mainnet
+    echo -e "\e[32m[+]\e[0m Enable prometheus monitoring"
     sed -i 's|^prometheus =.*|prometheus = "true"|g' $HOME/config/config.toml
     sed -i 's|^max_open_connections =.*|max_open_connections = 100|g' $HOME/config/config.toml
 
@@ -25,9 +31,10 @@ if [ ! -d "$HOME/data" ] || [ -z "$(ls -A $HOME/data)" ]; then
     # Download snapshot
     ## See: https://docs.polygon.technology/pos/how-to/snapshots/#downloading-and-using-client-snapshots
     rm -rf $HOME/data
-    echo "Downloading snapshot ..."
-    curl -L https://snapshot-download.polygon.technology/snapdown.sh | bash -s -- --network mainnet --client heimdall --extract-dir $HOME/data --validate-checksum true
+    echo -e "\e[32m[+]\e[0m Downloading snapshot"
+    curl -sL https://snapshot-download.polygon.technology/snapdown.sh | sed '/sudo apt-get update -y/d; /sudo apt-get install -y zstd pv aria2/d' | bash -s -- --network mainnet --client heimdall --extract-dir $HOME/data --validate-checksum true > $HOME/snapshot.log
 fi
 
 # Start heimdalld
+echo -e "\e[32m[+]\e[0m Start heimdalld..."
 heimdalld start --home $HOME --rest-server --chain mainnet
